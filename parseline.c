ae00faf908e479d2874df0268905742ac9df5da6
James Ngo <jngo21@calpoly.edu>
2019-03-05 17:21:34 -0800

    Passes all harness cases

---
+++ parseline.c
@@ -0,0 +1,194 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include "parseline.h"
+#define LNMAX 512
+#define PROMPT 6
+#define NULLERR 21
+#define LENERR 39
+#define INERR 21
+#define OUTERR 22
+#define ARGS_MAX 10
+#define PIPELINE_MAX 10
+
+int main(int argc, char *argv[]) {
+	int num, arg_count, cur_stage = 0, i = 0, in_redir = 0, out_redir = 0;
+	int j;
+	struct stage *stages = (struct stage*)calloc(PIPELINE_MAX,
+		sizeof(struct stage));
+	char *argv_list[ARGS_MAX];
+	char cur_arg[LNMAX];
+	char in[LNMAX];
+	char out[LNMAX];
+	char cmdlne[LNMAX + 1];
+	char *marker;
+	write(STDOUT_FILENO, "line: ", PROMPT);
+	if (1 >= (num = read(STDIN_FILENO, cmdlne, LNMAX + 1))) {
+		write(STDERR_FILENO, "invalid null command\n", NULLERR);
+		return 1;	
+	}
+	else if (num > LNMAX) {
+		write(STDERR_FILENO,
+			"command line length exceeds max length\n", LENERR);
+		return 2;
+	}
+	cmdlne[strlen(cmdlne) - 1] = '\0';
+	strtok(cmdlne, " ");
+	while (strtok(NULL, " ")) {
+		/* twiddle */
+	}
+	marker = cmdlne;
+	strcpy(in, "original stdin");
+	strcpy(out, "original stdout");
+	while (i < num) {
+		if (cmdlne[i] == '|' || num - i == 1) {
+			if (cmdlne[i] == '|' && !out_redir) {
+				sprintf(out, "pipe to stage %d", cur_stage + 1);
+			}
+			else if (cmdlne[i] == '|' && out_redir) {
+				fprintf(stderr, "%s: ambiguous output\n",
+					argv_list[0]);
+				for (j = 0; j < arg_count; j++) {
+					free(argv_list[j]);
+				}
+				return 5;
+			}
+			if (arg_count < 1) {
+				write(STDERR_FILENO, "invalid null command\n",
+					NULLERR);
+				return 1;	
+			}
+			for (j = 0; j < i; j++) {
+				if (marker[j] == '\0') {
+					marker[j] = ' ';
+				}
+			}
+			cmdlne[i++] = '\0';
+			stages[cur_stage].stage = cur_stage;
+			stages[cur_stage].pipeline = (char*)malloc(sizeof(char)
+				* strlen(marker));
+			stages[cur_stage].in = (char*)malloc(sizeof(char) *
+				strlen(in));
+			stages[cur_stage].out = (char*)malloc(sizeof(char) *
+				strlen(out));
+			strcpy(stages[cur_stage].pipeline, marker);
+			stages[cur_stage].argc = arg_count;
+			for (j = 0; j < stages[cur_stage].argc; j++) {
+				stages[cur_stage].argv_list[j] = (char*)malloc(
+					sizeof(char) * strlen(argv_list[j]));
+				strcpy(stages[cur_stage].argv_list[j],
+					argv_list[j]);
+			}
+			strcpy(stages[cur_stage].in, in);
+			strcpy(stages[cur_stage].out, out);
+			marker = marker + strlen(marker) + 1;
+			sprintf(in, "pipe from stage %d", cur_stage);
+			strcpy(out, "original stdout");
+			in_redir = 1;
+			out_redir = 0;
+			arg_count = 0;
+			strtok(cmdlne, " ");
+			while (strtok(NULL, " ")) {
+				/* twiddle */
+			}
+			cur_stage++;
+		}
+		else if (cmdlne[i] == '<') {
+			if (in_redir++) {
+				write(STDERR_FILENO, "bad input redirection\n",
+					INERR);
+				return 3;
+			}
+			else {
+				while (cmdlne[++i] == '\0' ||
+					cmdlne[i] == ' ') {
+					/* twiddle */
+				}
+				strcpy(in, cmdlne + i);
+				for (j = 0; in[j] != '\0' &&
+					in[j] != '<' && in[j] != '>'
+					&& in[j] != '|'; j++) {
+					/* twiddle */
+				}
+				in[j] = '\0';
+				i += strlen(in);
+			}
+		}
+		else if (cmdlne[i] == '>') {
+			if (out_redir++) {
+				write(STDERR_FILENO, "bad output redirection\n",
+					OUTERR);
+				return 4;
+			}
+			else {
+				while (cmdlne[++i] == '\0' ||
+					cmdlne[i] == ' ') {
+					/* twiddle */
+				}
+				strcpy(out, cmdlne + i);
+				for (j = 0; out[j] != '\0' &&
+					out[j] != '<' && out[j] != '>'
+					&& out[j] != '|'; j++) {
+					/* twiddle */
+				}
+				out[j] = '\0';
+				i += strlen(out);
+			}
+		}
+		else if (cmdlne[i] == '\0' || cmdlne[i] == ' ') {
+			i++;
+		}
+		else {
+			if (arg_count == ARGS_MAX) {
+				fprintf(stderr, "maximum pipeline argument"
+					        "length exceeded\n");
+				for (j = 0; j < arg_count; j++) {
+					free(argv_list[j]);
+				}
+				return 7;
+			}
+			while (cmdlne[i] == '\0') {
+				i++;
+			}
+			strcpy(cur_arg, cmdlne + i);
+			for (j = 0; cur_arg[j] != '\0' && cur_arg[j] != '<' &&
+				cur_arg[j] != '>' && cur_arg[j] != '|' &&
+				cur_arg[j] != ' '; j++) {
+				/* twiddle */
+			}
+			cur_arg[j] = '\0';
+			argv_list[arg_count] = (char*)malloc(sizeof(char)
+				* (strlen(cur_arg) + 1));
+			strcpy(argv_list[arg_count], cur_arg);
+			i += strlen(cur_arg);
+			arg_count++;
+		}
+	}
+
+	print_stage(stages, cur_stage); 
+	return 0;
+}
+
+void print_stage(struct stage *stages, int num_stages) {
+	int i, j;
+	for (i = 0; i < num_stages; i++) {
+		printf("\n--------\n"
+		       "Stage %d: \"%s\"\n"
+       	               "--------\n", stages[i].stage, stages[i].pipeline);
+		printf("     input: %s\n", stages[i].in);
+		printf("    output: %s\n", stages[i].out);
+		printf("      argc: %d\n", stages[i].argc);
+		printf("      argv: ");
+		for (j = 0; j < stages[i].argc; j++) {
+			printf("\"%s\"", stages[i].argv_list[j]);
+			free(stages[i].argv_list[j]);
+			if (j != stages[i].argc - 1) {
+				printf(",");
+			}
+			else {
+				printf("\n");
+			}
+		}
+	}	
+}
---
+++ parseline.h
@@ -0,0 +1,15 @@
+#ifndef PARSELINE_H
+#define PARSELINE_H
+
+struct stage {
+	int stage;
+	char *pipeline;
+	int argc;
+	char *in;
+	char *out;
+	char *argv_list[10];
+};
+
+void print_stage(struct stage*, int);
+
+#endif
